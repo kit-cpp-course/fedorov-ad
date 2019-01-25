4b7f8445752da34038587934784aab57baa3ff94
Andrey <46047628+deystroyter@users.noreply.github.com>
2019-01-25 10:36:14 +0300

    Add files via upload

--- mainwindow.h
+++ mainwindow.h
@@ -8,44 +8,50 @@
 #include "include.h"
 #include <QMessageBox>
 
+
 namespace Ui {
-
-class MainWindow;
-
+    class MainWindow;
 }
-
-
-
+//наследуем от QMainWindow
 class MainWindow : public QMainWindow
 {
-    Q_OBJECT
+    Q_OBJECT //обязательный макрос для реализации слотов и сигналов
 
 public:
     explicit MainWindow(QWidget *parent = 0);
     ~MainWindow();
 
+//слоты - ждут своего сигнала - выполняют, что умеют
 private slots:
 
-    void on_pb00_01_clicked();
-    void on_pb00_0_clicked();
-    void on_pb00_1_clicked();
-    void on_pb00_2_clicked();
-    void on_pb00_3_clicked();
-    void on_pb00_4_clicked();
-    void on_pb00_5_clicked();
-    void on_pb00_6_clicked();
-    void on_pb00_7_clicked();
-    void on_pb00_8_clicked();
-    void on_pb00_9_clicked();
+    void on_pb00_01_clicked(); //клик "-" передаёт 99 в void click__on_pb(int num)
+    void on_pb00_0_clicked(); //клик "0" передаёт 0 в void click__on_pb(int num)
+    void on_pb00_1_clicked(); //клик "1" передаёт 1 в void click__on_pb(int num)
+    void on_pb00_2_clicked(); //клик "2" передаёт 2 в void click__on_pb(int num)
+    void on_pb00_3_clicked(); //клик "3" передаёт 3 в void click__on_pb(int num)
+    void on_pb00_4_clicked(); //клик "4" передаёт 4 в void click__on_pb(int num)
+    void on_pb00_5_clicked(); //клик "5" передаёт 5 в void click__on_pb(int num)
+    void on_pb00_6_clicked(); //клик "6" передаёт 6 в void click__on_pb(int num)
+    void on_pb00_7_clicked(); //клик "7" передаёт 7 в void click__on_pb(int num)
+    void on_pb00_8_clicked(); //клик "8" передаёт 8 в void click__on_pb(int num)
+    void on_pb00_9_clicked(); //клик "9" передаёт 9 в void click__on_pb(int num)
+
+    //действие при клике на ячейку таблицы (запоминает строку + столбец, ещё выводит их в label)
     void on_table_cellClicked(int row, int column);
+    //действие при клике кнопки решения (обнуление матрицы + её решение, вывод решения)
     void on_solve_clicked();
+    //действие при нажатии на "Новая игра", сброс, генерация, вывод
     void on_actionNew_game_triggered();
+    //действие при нажатии "Выход", закрытие приложения
     void on_actionExitQ_triggered();
+    //действие при нажатии "Очистить всё", чистит поле, выводит начальное условие ("константы")
     void on_pushButton_clicked();
 
 private:
     Ui::MainWindow *ui;
+    //инты со значением актуальных столбца и строки
     int Column, Row;
+    //клики на кнопки "0-9" + "-", запись их в матрицу
     void click__on_pb(int num);
 };
 
--- matrix.h
+++ matrix.h
@@ -5,13 +5,20 @@
 #endif
 
 namespace sudoku{
+    //класс матрица
     class matrix{
         public:
+        //функция проверки победы
         bool you_win();
+        //функция записи значения в матрицу, где i, j - индексы, val - значение
         void write(int i, int j, int val);
+        //функция создания нового игрового поля
         void new_puzzle();
+        //функция решения матрицы solve() в init() в dfs(0,0)
         void solve();
+        //функция сброса пользовательских изменений
         void reset();
+        //функция считывания значений в сгенерированной матрице
         int read(int i, int j);
         int output(int (*mat)[10]);
         int input(int (*mat)[10]);
@@ -19,14 +26,19 @@ namespace sudoku{
         matrix();
 
         private:
+        //рабочая матрица
         int obj[10][10];
         int row[10];
         int col[10];
         int block[5][5];
+        //матрица с "константами"
         int ne[10][10];
         sudoku::rdarray ra;
+        //ф-ция составления игровой таблицы
         int dfs(int ni, int nj);
+        //функция полного обнуления, в т.ч массивов
         void empty();
+        //
         void init();
     };
 }
